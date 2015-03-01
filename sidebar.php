210da4552cbaa9410ff9227969e066b7a3adc977
Alex Lillo <alex.lillo@gmail.com>
2015-03-01 11:08:02 +0100

    search and navigation styling

--- sidebar.php
+++ sidebar.php
@@ -5,38 +5,5 @@
       <?php dynamic_sidebar( 'sidebar-1' ); ?>
     </div><!-- #primary-sidebar -->
   <?php endif; ?>
-   	            
-
-            
-            <div>
-            <script src="http://widgets.twimg.com/j/2/widget.js"></script>
-            <script>
-            new TWTR.Widget({
-              version: 2,
-              type: 'profile',
-              rpp: 4,
-              interval: 30000,
-              width: 240,
-              height: 300,
-              theme: {
-                shell: {
-                  background: '#dedede',
-                  color: '#242424'
-                },
-                tweets: {
-                  background: '#f2f3f4',
-                  color: '#666666',
-                  links: '#cc6633'
-                }
-              },
-              features: {
-                scrollbar: false,
-                loop: false,
-                live: false,
-                behavior: 'all'
-              }
-            }).render().setUser('joseandom').start();
-            </script>
-            </div>
 
 </aside>
